aebbf7ac8e0b7ef4cbc58698908b6e00ac6fc5e9
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-06-05 11:39:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors")
+require("./db/config");
+const User = require("./model/User");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password
+    resp.send(result);
+})
+
+app.post("/login", async (req, resp) => {
+    if (req.body.email && req.body.password) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user)
+        }
+        else {
+            resp.send({ result: 'No User Found' })
+        }
+    } else {
+        resp.send({ result: 'No User Found' })
+    }
+})
+
+app.listen(5000)
