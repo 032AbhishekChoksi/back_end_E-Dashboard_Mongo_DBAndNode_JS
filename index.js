1788bd73211378d41fb8d58a8d8fbacdc9a652b3
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-06-06 12:49:21 +0530

    React and node JS project in Hindi #24 Delete Product API

--- index.js
+++ index.js
@@ -45,4 +45,9 @@ app.get("/products", async (req, resp) => {
     }
 })
 
+app.delete("/product/:id", async (req, resp) => {
+    const result = await Product.deleteOne({_id:req.params.id})
+    resp.send(result)
+})
+
 app.listen(5000)
